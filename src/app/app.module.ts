2e7e9c0504e702a8b8aa2c5c52aa24dcd6460fed
anlinpanda <anlin_c@yahoo.ca>
2019-01-29 00:43:40 -0800

    favicon and info changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { InfoComponent } from './info/info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule,
